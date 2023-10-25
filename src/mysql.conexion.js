69a817422e261f8dd31bb333f5719498366dd9df
Pablo1864 <pablonpenaloza@sanluis.edu.ar>
2023-10-24 22:28:06 -0300

    gestion examen

--- src/mysql.conexion.js
+++ src/mysql.conexion.js
@@ -1,57 +1,57 @@
 import mysql from 'mysql';
 let todosPacientes;
 
-const conexion= mysql.createConnection(
+const conexion = mysql.createConnection(
     {
-        user:'root',
-        host:'localhost',
-        password:'',
-        database:'lab'
+        user: 'root',
+        host: 'localhost',
+        password: '',
+        database: 'labanalisis'
     }
 )
 
-const conectar=() =>{
+const conectar = () => {
     conexion.connect(err => {
-        if(err) throw err 
+        if (err) throw err
         console.log('conectado')
     })
 };
 
 //si filed.lenght es mayor a cero hay algun registro mal, ver capturar la excepción
 //AGREGAR PACIENTE
- const agregarPaciente = (nombre,apellido,dni,telefono,sexo,fechaNacimiento,email,provincia,localidad,domicilio,
+const agregarPaciente = (nombre, apellido, dni, telefono, sexo, fechaNacimiento, email, provincia, localidad, domicilio,
     obraSocial, nroAfiliado) => {
-   const sql = `INSERT INTO pacientes(idPaciente,nombre,apellido,dni,fechaNacimiento,provincia,localidad,domicilio,email,telefono,sexo,obraSocial,nroAfiliado)
+    const sql = `INSERT INTO pacientes(idPaciente,nombre,apellido,dni,fechaNacimiento,provincia,localidad,domicilio,email,telefono,sexo,obraSocial,nroAfiliado)
 VALUES (${null},"${nombre}","${apellido}",${dni},"${fechaNacimiento}","${provincia}","${localidad}","${domicilio}","${email}",${telefono},"${sexo}","${obraSocial}",${nroAfiliado})`;
-   conexion.query(sql, function (err, result, filed) {
-     if (err) throw err;
-     console.log(result);
-   });
- };
+    conexion.query(sql, function (err, result, filed) {
+        if (err) throw err;
+        console.log(result);
+    });
+};
 
- 
+
 //OBTENER PACIENTES
-const obtenerPacientes=()=>{
-    const sql= 'SELECT idPaciente, nombre, apellido, fechaNacimiento FROM pacientes'
-    conexion.query(sql, function(err, result, field){
-        if(result){
-            todosPacientes= result;
+const obtenerPacientes = () => {
+    const sql = 'SELECT idPaciente, nombre, apellido, fechaNacimiento FROM pacientes'
+    conexion.query(sql, function (err, result, field) {
+        if (result) {
+            todosPacientes = result;
         }
         else console.log(err)
     })
-return todosPacientes //array de objetos
+    return todosPacientes //array de objetos
 
 }
 
 
 //BORRAR PACIENTE
-const borrarPaciente =id =>{
-    const sql= `DELETE FROM pacientes WHERE idPaciente=${id}`
+const borrarPaciente = id => {
+    const sql = `DELETE FROM pacientes WHERE idPaciente=${id}`
     conexion.query(sql)
 }
 
 //BUSCAR PACIENTE BY ID
-const buscarPacientePorId = id =>{
+const buscarPacientePorId = id => {
     return new Promise((resolve, reject) => {
         const sql = `SELECT * FROM pacientes WHERE idPaciente=${id}`;
         conexion.query(sql, (err, res, field) => {
@@ -61,18 +61,28 @@ const buscarPacientePorId = id =>{
 }
 
 //BUSCAR ORDER BY ID
-const buscarOrdenPorID = id =>{
+const buscarOrdenPorID = id => {
     return new Promise((resolve, reject) => {
         const sql = `SELECT * FROM ordenes WHERE nroOrden=${id}`;
         conexion.query(sql, (err, res, field) => {
-            if (res){
+            if (res) {
                 resolve(res)
             } else {
                 reject(err);
             }
         });
     })
-    
-}
 
-export{conectar,agregarPaciente,obtenerPacientes,borrarPaciente, buscarOrdenPorID}
+}
+const insertarExamen = (nuevoExamen, callback) => {
+    conexion.query('INSERT INTO examenes SET ?', nuevoExamen, (error, results, fields) => {
+        if (error) {
+            console.error('Error al insertar el examen:', error);
+        } else {
+            console.log('Examen insertado con éxito');
+        }
+        callback(error, results);
+    });
+};
+
+export { conectar, agregarPaciente, obtenerPacientes, borrarPaciente, buscarOrdenPorID, insertarExamen }
